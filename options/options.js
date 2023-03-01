3d9fbc2d7ccd371ce74e6f24aaadfbbf1e6eb604
Crowdbotics <engineering@crowdbotics.com>
2023-03-01 11:13:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-dewfervgwebe-de-67220",
+  url: "https://tst-dewfervgwebe-de-67220.botics.co",
+  api: "https://tst-dewfervgwebe-de-67220.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
